5e0357fd180c124d7af2f2e504597a89e4dea584
Vladimir Danilov <dev.danilovl@gmail.com>
2022-11-10 16:14:50 +0100

    Add docker.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 const path = require('path')
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,8 +10,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
     }
-  },
-  server: {
-    open: './public/index.html'
   }
 })
